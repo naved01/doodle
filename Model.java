593e38a230f4ab078a6978cfb5c72c51b9ab4ee1
Ahmad Naved <naved993@gmail.com>
2016-02-18 15:02:26 -0500

    set up basic MVC architecture

---
+++ Model.java
@@ -0,0 +1,7 @@
+import java.util.Observable;
+
+public class Model extends Observable {
+    
+    
+    
+}
---
+++ ThicknessPicker.java
@@ -0,0 +1,12 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class ThicknessPicker implements Observer {
+    
+    
+	// Observer interface 
+	@Override
+	public void update(Observable arg0, Object arg1) {
+        //override code	
+	}
+}
