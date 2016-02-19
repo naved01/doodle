db9f790e7c6606c7de153f8a76815063ebe0fcc2
Ahmad Naved <naved993@gmail.com>
2016-02-18 19:06:59 -0500

    defined Model class

--- Model.java
+++ Model.java
@@ -1,6 +1,32 @@
 import java.util.Observable;
+import java.util.ArrayList;
 
 public class Model extends Observable {
     
     //variables
+    private String currentColor;
+    private ArrayList<Stroke> strokes;
+    
+    //constructors 
+    public Model() {
+        currentColor = "black";
+        strokes = new ArrayList<Stroke>();
+    }
+    
+    //methods
+    public void addStroke(Stroke s) {
+        strokes.add(s);
+    }
+    
+    public ArrayList<Stroke> getStrokes() {
+        return strokes;
+    }
+    
+    public void setCurrentColor(String currentColor) {
+        this.currentColor = currentColor;
+    }
+    
+    public String getCurrentColor() {
+        return currentColor;
+    }
 }
